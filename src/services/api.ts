9cfbeeee1d1484f887e7f34b220a9cbc5c51d301
ankh1erdene <ankh1erdene@gmail.com>
2026-01-09 20:11:10 +0800

    api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,25 @@ import { orders as mockOrders } from '../mockData'
 const API_BASE_URL = 'http://localhost:3001/api'
 
 export async function fetchOrders(): Promise<Order[]> {
-  return Promise.resolve([...mockOrders])
+  let response;
+  try {
+     response = await fetch(`${API_BASE_URL}/orders`)
+    if (response.status !=200  ) {
+      throw new Error('Network response was not ok')
+    }
+    console.log(response, "response");
+    const data = await response.json()
+    return data.orders.map((o:any) => ({
+      ...o,
+      items: o.items ?? [], // API has none -> []
+    })) as Order[];
+  }catch(err){
+    console.log(err, "err")
+    return Promise.resolve([...mockOrders])
+  }
+  // return fetch(`${API_BASE_URL}/orders`)
+  // return Promise.resolve([...mockOrders])
+
 }
 
 export async function updateOrderStatus(orderId: string, status: Order['status']): Promise<Order> {
@@ -13,5 +31,19 @@ export async function updateOrderStatus(orderId: string, status: Order['status']
     throw new Error('Order not found')
   }
   order.status = status
+  try{
+    const result = await fetch(`${API_BASE_URL}/orders/${orderId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ status }),
+    })
+    if(result.status !=200){
+      throw new Error('Network response was not ok')
+    }
+  }catch(err){
+    console.log(err, "err")
+  }
   return Promise.resolve(order)
 }
